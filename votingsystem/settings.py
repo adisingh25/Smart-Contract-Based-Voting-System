3059dd76f05e94da2aa96de2aa2b08afbdb8fb61
kpawan1009 <103193620+kpawan1009@users.noreply.github.com>
2022-06-08 23:52:47 +0530

    2nd commit

--- votingsystem/settings.py
+++ votingsystem/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p_!*u)q*skn@i*l&5=#dp8v-@^csi%1^kd3n4cz*aquzl#25vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
