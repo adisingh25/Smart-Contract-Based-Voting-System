fc52257d93919d52032775d45709cda77e0a761e
kpawan1009 <103193620+kpawan1009@users.noreply.github.com>
2022-06-08 23:49:33 +0530

    2nd commit

--- home/sendsms.py
+++ home/sendsms.py
@@ -1,8 +1,6 @@
 from twilio.rest import Client
 
 def sendsms(mess):
-    account_sid = 'ACfa814df1d6fa8277160942dbbee3d51a'
-    auth_token = '9abe2acc167a8869fe64ab644a7d1062'
     client = Client(account_sid, auth_token)
     message = client.messages \
                         .create(
